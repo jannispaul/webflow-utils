074b93bd3379de73a3d9c39fdc37b01d80b35ca4
jannispaul <jannis@pixel-pink.de>
2024-09-17 10:44:45 +0200

    fixed lazy loading for non autoplay videos

--- utils/smart-video.js
+++ utils/smart-video.js
@@ -1,5 +1,6 @@
-// Videos need to have autoplay and data-src attribute to work
+// Videos need to have data-src attribute to work
 // Optional:
+// autoplay for smart autoplay, otherwise only lazy loading
 // data-breakpount -> on script tag, if you want to load video on mobile
 // data-src-mobile -> if you want to load a different video on mobile (max-width: 768px)
 // data-poster-mobile -> if you want to use a different poster on mobile
@@ -9,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Get all videos with source data-src (relies on css )
   let lazyVideos = Array.from(document.querySelectorAll("video:has(source[data-src])"));
   let autoPlayVideos = Array.from(document.querySelectorAll("video[autoplay]"));
+  let allVideos = lazyVideos.concat(autoPlayVideos);
   let isMobile;
   // Get all script elements with data-smart-video
   let currentScriptTag = document.querySelector("script[data-smart-video]");
@@ -23,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Make sure intersection observer is available
   if ("IntersectionObserver" in window) {
     // Set up intersection observer for lazy load videos
-    let lazyVideoObserver = new IntersectionObserver(
+    let lazyLoadObserver = new IntersectionObserver(
       (entries, observer) => {
         entries.forEach((video) => {
           if (video.isIntersecting) {
@@ -33,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             // Unobserve the video if it doesn't have the 'replay' attribute
           } else if (video.target.hasAttribute("loaded")) {
-            lazyVideoObserver.unobserve(video.target);
+            lazyLoadObserver.unobserve(video.target);
           }
         });
       },
@@ -73,17 +75,19 @@ document.addEventListener("DOMContentLoaded", function () {
       playbackObserver.unobserve(video);
     }
 
-    autoPlayVideos.forEach((video) => {
+    allVideos.forEach((video) => {
       console.log("auto play", video);
       // check if source has data-src
       setPosterSource(video);
       // if video is not loaded and source has data-src create lazy loading observer
       if (!video.hasAttribute("loaded") && Array.from(video.children).some((source) => source.hasAttribute("data-src"))) {
         console.log("observing", video);
-        lazyVideoObserver.observe(video);
+        lazyLoadObserver.observe(video);
       }
-      // Oberse all videos for play and pause
-      playbackObserver.observe(video);
+      if (video.hasAttribute("autoplay")) {
+        // Oberse only autoplay videos for play and pause
+        playbackObserver.observe(video);
+      }
     });
   }
   function setSource(video) {
